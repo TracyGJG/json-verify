b3f96d6869d3e50aea90a7165a5faa145588631b
tgjgilmore <tracyg.gilmore@gmail.com>
2022-11-27 21:24:32 +0000

    Tokens processed

--- json-verify.js
+++ json-verify.js
@@ -40,7 +40,6 @@ function parseAll(data) {
 
 function verify(input, indentType = '\t') {
 	let data = input;
-	let error = '';
 	const tokenStack = [];
 	let tokenError = '';
 	const report = {
@@ -51,61 +50,119 @@ function verify(input, indentType = '\t') {
 			: indentType,
 	};
 
-	console.log(
-		'TYPE'.padEnd(18, ' '),
-		'VALUE'.padEnd(20, ' '),
-		'DATA remaining',
-		`\n${'-'.repeat(60)}`
-	);
+	// console.log(
+	// 	'TYPE'.padEnd(18, ' '),
+	// 	'VALUE'.padEnd(20, ' '),
+	// 	'DATA remaining',
+	// 	`\n${'-'.repeat(60)}`
+	// );
 	while (data?.length && !tokenError) {
 		const { type, value, remainder } = parseAll(data);
 		data = remainder;
-		tokenError = processToken(tokenStack, type, value, report);
-		// error = type === 'Error' ? value : '';
-		console.log(type.padEnd(18, ' '), value.padEnd(20, ' '), data);
+		tokenError =
+			type === 'Error'
+				? value
+				: processToken(tokenStack, type, value, report);
+		// console.log(type.padEnd(18, ' '), value.padEnd(20, ' '), data);
 	}
-
-	return report.json.join('\n');
+	if (!tokenError && (data?.length || tokenStack.length)) {
+		tokenError = 'Unexpected end of source data';
+	}
+	return [...report.json, tokenError].join('\n');
 }
 
 function processToken(stack, token, value, _report) {
 	const STACK_IS_EMPTY = !stack.length;
+	const STACK_TYPE = {
+		ARRAY: 'ARRAY',
+		OBJECT: 'OBJECT',
+	};
+	const isStep = (...steps) => !steps.length || steps.includes(stack[0].step);
+	const isARRAY = (...steps) =>
+		isStep(...steps) && stack.length && stack[0].type === STACK_TYPE.ARRAY;
+	const isOBJECT = (...steps) =>
+		isStep(...steps) && stack.length && stack[0].type === STACK_TYPE.OBJECT;
 	let tokenError = '';
 	const fallbackTokenReport = datum => report(datum, false, false, 0);
 	const openTokenReport = datum => report(datum, false, true, 1);
 	const closeTokenReport = datum => report(datum, true, false, -1);
+	const commaTokenReport = datum => report(datum, false, true, 0);
+	const colonTokenReport = datum => report(`${datum} `, false, false, 0);
 	const tokenReports = {
 		OpenArray: openTokenReport,
 		OpenObject: openTokenReport,
 		CloseArray: closeTokenReport,
 		CloseObject: closeTokenReport,
-		SeparatingComma: datum => report(datum, false, true, 0),
-		PropertyColon: datum => report(`${datum} `, false, false, 0),
+		SeparatingComma: commaTokenReport,
+		PropertyColon: colonTokenReport,
 	};
 
-	/* Verify JSON Token
-	TOKEN		VERIFICATION			ACTION					tokenError
+	const primitiveVerifier = () => STACK_IS_EMPTY || isStep(2) || isARRAY(0);
+	const propertyVerifier = () => STACK_IS_EMPTY || isStep(0, 2);
+	const tokenVerifiers = {
+		Null: primitiveVerifier,
+		Boolean: primitiveVerifier,
+		Number: primitiveVerifier,
+		String: propertyVerifier,
+		OpenArray: primitiveVerifier,
+		CloseArray: () => isARRAY(0, 1),
+		PropertyColon: () => isOBJECT(1),
+		SeparatingComma: () => isARRAY(1) || isOBJECT(3),
+		OpenObject: primitiveVerifier,
+		CloseObject: () => isOBJECT(0, 3),
+	};
 
-	Null		STACK_IS_EMPTY | ARRAY (0/2) | OBJECT 2			-> 'Unexpected Null encountered'		
-	Boolean		STACK_IS_EMPTY | ARRAY (0/2) | OBJECT 2			-> 'Unexpected Boolean encountered'		
-	Number		STACK_IS_EMPTY | ARRAY (0/2) | OBJECT 2			-> 'Unexpected Number encountered'		
-	String		STACK_IS_EMPTY | ARRAY (0/2) | OBJECT (0/2)		-> 'Unexpected String encountered'
-	[			STACK_IS_EMPTY | ARRAY (0/2) | OBJECT 2
-										=> UNSHIFT ARRAY 0		-> "Unexpected start of Array"
-	]			ARRAY (0/1)				=> SHIFT				-> "Unexpected end of Array"
-	:			OBJECT 1				=> INC					-> "Unexpected colon encountered"
-	,			ARRAY 1 | OBJECT 3		=> INC					-> "Unexpected comma encountered"
-	{			STACK_IS_EMPTY | ARRAY (0/2) | OBJECT 2
-										=> UNSHIFT OBJECT 0		-> "Unexpected start of object"
-	}			OBJECT (0|3)			=> SHIFT				-> "Unexpected end of object"
+	const incStep = (restStr = '') => (
+		stack.length && stack[0].step++, restStr
+	);
+	const tokenErrors = {
+		Null: isVerified =>
+			isVerified ? incStep() : 'Unexpected Null encountered',
+		Boolean: isVerified =>
+			isVerified ? incStep() : 'Unexpected Boolean encountered',
+		Number: isVerified =>
+			isVerified ? incStep() : 'Unexpected Number encountered',
+		String: isVerified =>
+			isVerified ? incStep() : 'Unexpected String encountered',
+		OpenArray: isVerified => {
+			if (isVerified) {
+				incStep();
+				stack.unshift({ type: STACK_TYPE.ARRAY, step: 0 });
+				return '';
+			}
+			return 'Unexpected Array start';
+		},
+		CloseArray: isVerified =>
+			isVerified ? (stack.shift(), '') : 'Unexpected Array end',
+		PropertyColon: isVerified =>
+			isVerified ? incStep() : 'Unexpected colon encountered',
+		SeparatingComma: isVerified =>
+			isVerified
+				? ((stack[0].step = 0), '')
+				: 'Unexpected comma encountered',
+		OpenObject: isVerified => {
+			if (isVerified) {
+				incStep();
+				stack.unshift({ type: STACK_TYPE.OBJECT, step: 0 });
+				return '';
+			}
+			return 'Unexpected Object start';
+		},
+		CloseObject: isVerified =>
+			isVerified ? (stack.shift(), '') : 'Unexpected Object end',
+	};
 
-*/
+	// console.log(
+	// 	`\n${stack.length}: ${
+	// 		stack.length ? JSON.stringify(stack[0]) : 'STACK EMPTY'
+	// 	}`
+	// );
+	const verificationResult = tokenVerifiers[token]();
+	// console.log('>', token, verificationResult);
+	tokenError = tokenErrors[token](verificationResult);
+	// console.log(stack.length ? JSON.stringify(stack[0]) : 'STACK EMPTY');
 
 	return tokenError || (tokenReports[token] || fallbackTokenReport)(value);
-
-	function isPrimitive(_token) {
-		return ['Null', 'Boolean', 'Number', 'String'].includes(_token);
-	}
 
 	function report(datum, preNewline, postNewline, indentDelta) {
 		preNewline && newLine(indentDelta);
@@ -127,8 +184,16 @@ function processToken(stack, token, value, _report) {
 // export default verify;
 
 const testData = [
-	' null true false, -42: 0.123    []42e-1 42E+2 "Hello World" {} ',
-	' null true false, -42, 0.123  []42e-1 42E+2 "Hello World" "unrecognisedTokenHere {} ',
+	' { "a" : [ false , true ] , "b" : { "c" : 42, "d" : "Hello World" }, "e": null } ',
+	' { "a" : [ false , true ] , "b" : { "c" : 42, "d" : "Hello World" }, "e": null } ',
+	' { "a" : [ false , true ] , "b" : { "c" : 42, "d" : "Hello World" }, "e": null  ',
 	' { "a" : [ "b" , "c" ] , "d" : { "e" : "f" } } ',
+	'{ "a": null, "_": {"b": true, "c": false }, "d": 42, "e": "Hello World" }',
+	'{ "a": null, "b": true, "c": false, "d": 42, "e": "Hello World" }',
+	'[ null, [ true, false], 42, "Hello World" ]',
+	'[ null, true, false, 42, "Hello World" ]',
+	' null true false, -42, 0.123  []42e-1 42E+2 "Hello World" "unrecognisedTokenHere {} ',
+	' null true false, -42: 0.123    []42e-1 42E+2 "Hello World" {} ',
 ];
-console.log(verify(testData[2], 2));
+
+console.log(verify(testData[0], 2));
