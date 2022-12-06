288886fa91bea6b8722302e325fb05b269962ed4
tgjgilmore <tracyg.gilmore@gmail.com>
2022-12-06 20:20:38 +0000

    Testing with initial verification tests

--- jsonVerify.js
+++ jsonVerify.js
@@ -1,48 +1,183 @@
-const jsonTokens = {
-	Null: { pattern: 'null' },
-	Boolean: { pattern: '(false|true)' },
-	Number: { pattern: '-?(0|[1-9]\\d*)(\\.\\d+)?([eE][+-]?\\d+)?' },
-	String: {
-		pattern: '"([^\\\\"\\x00-\\x1f]|\\\\(["\\/bfnrt]|u[\\da-fA-F]{4}))*"',
+//#region Preparation functions
+export function createJsonState(sourceData) {
+	return {
+		report: '',
+		error: '',
+		remainder: sourceData.trim(),
+	};
+}
+
+export function createIndentFunction(indent = '\t') {
+	const indentText = `${indent}`;
+	const indentSpaces = +indentText > -1 ? ' '.repeat(+indentText) : '\t';
+	return numIntents =>
+		(/^-?\d+$/.test(indentText) ? indentSpaces : indent).repeat(numIntents);
+}
+//#endregion
+
+//#region create jsonTokeniser
+function createJsonTokeniser(jsonTokenPatterns) {
+	const jsonRegExpObject = new RegExp(
+		`^(${Object.entries(jsonTokenPatterns)
+			.map(([_token, _value]) => `(?<${_token.replace(' ', '_')}>${_value})`)
+			.join('|')})\\s*(?<remainder>.*)$`
+	);
+
+	return function (jsonString) {
+		const tokenMatches = jsonString.remainder.match(jsonRegExpObject)
+			?.groups || {
+			Error: 'Unrecognised content encountered',
+			remainder: jsonString,
+		};
+		const tokenValue = Object.entries(tokenMatches).reduce(extractTokens, {});
+		console.log('tokenized', { tokenValue });
+		jsonString.remainder = tokenMatches.remainder;
+		return tokenValue;
+	};
+
+	function extractTokens(obj, [key, value]) {
+		return key === 'remainder'
+			? { ...obj, remainder: value }
+			: value?.length
+			? { ...obj, token: key.replace('_', ' '), value }
+			: obj;
+	}
+}
+
+export const jsonTokenizer = createJsonTokeniser({
+	Null: 'null',
+	Boolean: '(false|true)',
+	Number: '-?(0|[1-9]\\d*)(\\.\\d+)?([eE][+-]?\\d+)?',
+	String: '"([^\\\\"\\x00-\\x1f]|\\\\(["\\\\/bfnrt]|u[\\da-fA-F]{4}))*"',
+	'Array opening': '\\[',
+	'Object opening': '\\{',
+	Comma: ',',
+	Colon: ':',
+	'Array closure': '\\]',
+	'Object closure': '\\}',
+});
+//#endregion
+
+//#region validateToken
+const OPENERS = ['Array_open', 'Object_open'];
+const CLOSURES = ['Array_close', 'Object_close'];
+const PRIMITIVES = ['Null', 'Boolean', 'Number', 'String'];
+const NEW_CONTEXT = token =>
+	token === 'Array_open'
+		? { context: 'ARRAY', step: 0 }
+		: { context: 'OBJECT', step: 0, properties: [] };
+const MATCHING_CONTEXT = (token, stack) =>
+	stack.length &&
+	CLOSURES.includes(token) &&
+	(stack[0].context === (token === 'Array_close') ? 'ARRAY' : 'OBJECT');
+const PUSH_STACK = (token, stack) =>
+	OPENERS.includes(token) && stack.unshift(NEW_CONTEXT(token));
+const POP_STACK = (token, stack) =>
+	MATCHING_CONTEXT(token, stack) && stack.shift();
+const UPDATE_STACK = (token, stack) => stack.length && stack[0].step++;
+const RESET_STACK = (token, stack) => stack.length && (stack[0].step = 1);
+
+export const tokenValidators = {
+	TOP_LEVEL: { validTokens: [...PRIMITIVES, ...OPENERS], action: PUSH_STACK },
+	ARRAY_0: {
+		validTokens: ['Array_close', ...PRIMITIVES, ...OPENERS],
+		action: (token, stack) => {
+			POP_STACK(token, stack);
+			UPDATE_STACK(token, stack);
+			PUSH_STACK(token, stack);
+		},
 	},
-	OpenArray: { pattern: '\\[' },
-	OpenObject: { pattern: '\\{' },
-	SeparatingComma: { pattern: ',' },
-	CloseArray: { pattern: '\\]' },
-	CloseObject: { pattern: '\\}' },
-	PropertyColon: { pattern: ':' },
+	ARRAY_1: {
+		validTokens: ['Array_close', 'Comma'],
+		action: (token, stack) => {
+			UPDATE_STACK(token, stack);
+			POP_STACK(token, stack);
+		},
+	},
+	ARRAY_2: {
+		validTokens: [...PRIMITIVES, ...OPENERS],
+		action: (token, stack) => {
+			UPDATE_STACK(token, stack);
+			RESET_STACK(token, stack);
+			PUSH_STACK(token, stack);
+		},
+	},
+	OBJECT_0: {
+		validTokens: ['Object_close', 'String'],
+		action: (token, stack) => {
+			UPDATE_STACK(token, stack);
+			POP_STACK(token, stack);
+		},
+	},
+	OBJECT_1: { validTokens: ['Colon'], action: UPDATE_STACK },
+	OBJECT_2: {
+		validTokens: [...PRIMITIVES, ...OPENERS],
+		action: (token, stack) => {
+			UPDATE_STACK(token, stack);
+			PUSH_STACK(token, stack);
+		},
+	},
+	OBJECT_3: {
+		validTokens: ['Object_close', 'Comma'],
+		action: (token, stack) => {
+			UPDATE_STACK(token, stack);
+			POP_STACK(token, stack);
+		},
+	},
+	OBJECT_4: { validTokens: ['String'], action: RESET_STACK },
 };
 
-const jsonRegExpString = `^(${Object.entries(jsonTokens)
-	.map(([_token, _value]) => `(?<${_token}>${_value.pattern})`)
-	.join('|')})\\s*(?<remainder>.*)$`;
+export function validateToken({ token }, tokenStack, state) {
+	if (token === 'Error') return;
 
-const jsonRegExpObject = new RegExp(jsonRegExpString);
+	const tokenIndex = tokenStack.length
+		? `${tokenStack[0].context}_${tokenStack[0].step}`
+		: 'TOP_LEVEL';
 
-export function jsonTokenize(jsonString) {
-	const tokenMatches = jsonString.match(jsonRegExpObject)?.groups || {
-		Error: 'Unrecognised content encountered',
-		remainder: jsonString,
-	};
-	const extractTokens = (obj, [key, val]) =>
-		key === 'remainder'
-			? { ...obj, [key]: val }
-			: val?.length // key other than remainder but value has length
-			? { ...obj, type: key, value: val }
-			: obj;
-	return Object.entries(tokenMatches).reduce(extractTokens, {});
-}
-
-function jsonVerify(sourceData, indentText = '\t') {
-	const retValue = { report: [], error: '', remainder: sourceData.trim() };
-
-	while (retValue.remainder.length && !retValue.error) {
-		const result = jsonTokenize(retValue.remainder);
-		retValue.report += `${result.type} = ${result.value}\n`;
-		retValue.error = result.type === 'Error' ? result.value : '';
-		retValue.remainder = result.remainder.trim();
+	if (tokenValidators[tokenIndex].validTokens.includes(token)) {
+		tokenValidators[tokenIndex].action(token, tokenStack);
+	} else {
+		state.error = 'Error';
+		state.value = `Unexpected ${token.replace('_', ' ')} encountered`;
 	}
-	return retValue;
+}
+//#endregion
+
+export function updateReport(tokenValue, tokenStack, jsonState, indent) {
+	if (jsonState.error) return;
+	const TOKEN_STACK_POPULATED = !!tokenStack.length;
+
+	const isComma = tokenValue.token === 'Comma';
+	const isContextStart = TOKEN_STACK_POPULATED && !tokenStack[0].step;
+	const isFirstPrimitive = TOKEN_STACK_POPULATED && tokenStack[0].step === 1;
+	const isContextTerminated = CLOSURES.includes(tokenValue.token);
+	const emptyContext = +(isContextTerminated && isContextStart);
+	const newLine = isFirstPrimitive ? '\n' : '';
+	const indentText = indent(+!!newLine * tokenStack.length + emptyContext);
+	const colonSpace = indent(+isComma);
+
+	jsonState.report += `${newLine}${indentText}${tokenValue.value}${colonSpace}`;
+
+	if (!TOKEN_STACK_POPULATED && jsonState.remainder) {
+		jsonState.error = `Data remaining after complete JSON block.`;
+		return;
+	}
+	if (TOKEN_STACK_POPULATED && !jsonState.remainder) {
+		jsonState.error = `Incomplete JSON block${
+			tokenStack.length > 1 ? 's' : ''
+		}.`;
+	}
 }
 
-export default jsonVerify;
+export default function jsonVerify(sourceData, indentText) {
+	const jsonState = createJsonState(sourceData);
+	const indent = createIndentFunction(indentText);
+	const tokenStack = [];
+
+	while (jsonState.remainder && !jsonState.error) {
+		const tokenValue = jsonTokenizer(jsonState);
+		validateToken(tokenValue, tokenStack, jsonState);
+		updateReport(tokenValue, tokenStack, jsonState, indent);
+	}
+	return jsonState;
+}
--- jsonVerify.test.js
+++ jsonVerify.test.js
@@ -1,35 +1,64 @@
-import verify from './jsonVerify';
-// import primitiveTestCases from './primitive-test-cases.json';
-// import arrayTestCases from './array-test-cases.json';
-import compoundTestCases from './compound-test-cases.json';
+import jsonVerify, {
+	createJsonState,
+	createIndentFunction,
+	jsonTokenizer,
+	validateToken,
+	updateReport,
+} from './jsonVerify';
+
+import indentTestCases from './testing/indent-test-cases.json';
+import tokenTestCases from './testing/token-test-cases.json';
+import validateTestCases from './testing/validate-test-cases.json';
+import reportTestCases from './testing/report-test-cases.json';
+import verifyTestCases from './testing/verify-test-cases.json';
 
 describe('JSON verify', () => {
-	// describe('Primitive', () => {
-	// 	test.each(primitiveTestCases)('$description', ({ input, output }) => {
-	// 		const indentParam = input.indentParam || undefined;
-	// 		const result = verify(input.jsonString, indentParam);
-	// 		expect(result).toEqual(output);
-	// 		console.log(output.error ?? JSON.parse(output.report));
-	// 	});
-	// });
-	// describe('Array', () => {
-	// 	test.each(arrayTestCases)('$description', ({ input, output }) => {
-	// 		const indentParam = input.indentParam || undefined;
-	// 		const result = verify(input.jsonString, indentParam);
-	// 		expect(result).toEqual(output);
-	// 		console.log(output.error ?? JSON.parse(output.report));
-	// 	});
-	// });
-	describe.only('Compound', () => {
-		test.each(compoundTestCases)('$description', ({ input, output }) => {
-			const indentParam = input.indentParam || undefined;
-			const result = verify(input.jsonString, indentParam);
-			expect(result).toEqual(output);
-			if (output.error) {
-				console.error(output.error);
-			} else {
-				console.log(output.report, JSON.parse(output.report));
-			}
+	describe('Calculate Indentation', () => {
+		test.each(indentTestCases)('$description', ({ input, output }) => {
+			const calculateIndentation = createIndentFunction(input);
+			expect(calculateIndentation(2)).toEqual(output);
 		});
+	});
+	describe('JSON Tokenizer', () => {
+		test.each(tokenTestCases)('$description', ({ input, output }) => {
+			const jsonState = createJsonState(input);
+			const result = jsonTokenizer(jsonState);
+			expect(result.token).toEqual(output.token);
+			expect(result.value).toEqual(output.value);
+		});
+	});
+	describe('Validate Token', () => {
+		test.each(validateTestCases)('$description', ({ input, output }) => {
+			validateToken(input.tokenValue, input.tokenStack, input.state);
+
+			expect(input.tokenStack.length).toEqual(output.tokenStack.length);
+			if (input.tokenStack.length) {
+				expect(input.tokenStack[0].context).toEqual(
+					output.tokenStack[0].context
+				);
+				expect(input.tokenStack[0].step).toEqual(output.tokenStack[0].step);
+			}
+			expect(input.state).toEqual(output.state);
+		});
+	});
+	describe('Update Report', () => {
+		const indent = indentSize => '  '.repeat(indentSize);
+		test.each(reportTestCases)(
+			'$description',
+			({ input: { tokenValue, tokenStack, state }, output }) => {
+				updateReport(tokenValue, tokenStack, state, indent);
+				expect(state.report).toEqual(output.report);
+				expect(state.error).toEqual(output.error);
+			}
+		);
+	});
+	describe.only('Verify JSON', () => {
+		test.each(verifyTestCases)(
+			'$description',
+			({ input: [json, indent], output }) => {
+				const results = jsonVerify(json, indent);
+				expect(results).toEqual(output);
+			}
+		);
 	});
 });
